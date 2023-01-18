3982181931b3c8b5bcbc2e8efa3a17802295eab3
Glitch (glitch-hello-website) <none>
2023-01-18 22:56:45 +0000

    💗🏣 Checkpoint ./teamContent.js:29312057/434 ./.glitch-assets:29312057/8325 ./all.css:29312057/8 ./teamStyle.css:29312057/82

--- teamContent.js
+++ teamContent.js
@@ -12,7 +12,7 @@ var content =
    type:       "Biography",
    name:       "Yaelin Hough",
    roles:      ["Lead Writer", "Researcher"],
-   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
+   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/Yaelin",
    statement:  '<b>Things I\'m proud of in my heritage:</b> I\'m proud of South Korea\'s ability to overcome its past hardships. Despite years under colonial rule and the subsequent destitution, South Korea has managed to cultivate a vibrant culture and a powerful democracy.<br><b>What I want to convey through TFE:</b> I hope that we are able to display the history and connections between all East and Southeast Asian cultures that exist beyond the common knowledge of the West.',
   },
   
@@ -20,7 +20,7 @@ var content =
    type:       "Biography",
    name:       "Amy Hwang",
    roles:      ["Website Designer", "Writer", "Researcher"],
-   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
+   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/Amy",
    statement:  '<b>Things I\'m proud of in my heritage:</b> Food, entertainment culture, and Korean culture of \'Jeong\'<br><b>What I want to convey through TFE:</b> To grow awareness of the current affairs in Asia and to be an advocate for my own country\'s culture(I wish to shwo that there are other appealing korean cultures other than the entertainment industry',
   },
   
@@ -28,7 +28,7 @@ var content =
    type:       "Biography",
    name:       "Kevin Yu",
    roles:      ["Lead Web Designer", "Writer", "Researcher"],
-   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
+   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/Kevin",
    statement:  '<b>Things I\'m proud of in my heritage:</b> Traditional Chinese stories about love, betrayal, and war.<br><b>What I want to convey through TFE:</b> China is much more than social credit memes or cheap goods. 1.4B people come with a plethora of microcosms, each with unique beliefs and problems. We need a more multifaceted view of the world\'s most populous country—not just world politics but life on the ground.',
   },
   
@@ -36,7 +36,7 @@ var content =
    type:       "Biography",
    name:       "Lily Wang",
    roles:      ["Art and Design Director"],
-   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
+   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/Lily",
    statement:  '<b>Things I\'m proud of in my heritage:</b> Seeing people around the world appreciate Asian culture through food, media, music, landscape and beautiful traditions.<br><b>What I want to convey through TFE:</b> I want to show the public that countries are far more complex than most common impressions by providing through research.',
   },
   
@@ -44,7 +44,7 @@ var content =
    type:       "Biography",
    name:       "Oliver Chan",
    roles:      ["Writer", "Researcher"],
-   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
+   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/Oliver",
    statement:  '<b>Things I\'m proud of in my heritage:</b> Dim Sum, Mahjong, and Egg Waffle<br><b>What I want to convey through TFE:</b> I wish to share my expriences in Asian culture and the side of Asian culture that non-Asians may not have been exposed to.',
   },
   
@@ -52,7 +52,7 @@ var content =
    type:       "Biography",
    name:       "Jinnie Prapakamol",
    roles:      ["Writer", "Researcher"],
-   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
+   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/Jinnie",
    statement:  '<b>Things I\'m proud of in my heritage:</b> Thai Food 🤤<br><b>What I want to convey through TFE:</b> Educating myself and other people about different cultures in the east asian community 🫡',
   },
   
@@ -60,16 +60,16 @@ var content =
    type:       "Biography",
    name:       "Leila Ng",
    roles:      ["Social Media Assistant"],
-   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
+   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/Leila",
    statement:  '<b>Things I\'m proud of in my heritage:</b> My Fujianese heritage—family-centered, root centered.<br><b>What I want to convey through TFE:</b> The abundance of \'the Asian heritage.\' To make Asians of different communities and different upbringings feel heard.',
   },
   
   {
    type:       "Biography",
    name:       "Nathan Leung",
-   roles:      ["Writer", "Researcher"],
-   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
-   statement:  '<b>Things I\'m proud of in my heritage:</b> Thai Food 🤤<br><b>What I want to convey through TFE:</b> Educating myself and other people about different cultures in the east asian community 🫡',
+   roles:      ["Social Media Assistant"],
+   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/Nathan",
+   statement:  '<b>Things I\'m proud of in my heritage:</b> I come from Hong Kong, where I live iwht a diverse population and can learn many different languages.<br><b>What I want to convey through TFE:</b> I joined this team to share the cultures of diffrent Asian countires with other people.',
   },
   
 ];
