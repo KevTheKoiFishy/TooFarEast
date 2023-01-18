f0757293ebf32f1e8e784077754d400a5ac80f52
Glitch (glitch-hello-website) <none>
2023-01-18 22:32:46 +0000

    🏥👚 Checkpoint ./teamStyle.css:29312057/36 ./all.css:29312057/24 ./teamContent.js:29312057/1673

--- teamContent.js
+++ teamContent.js
@@ -13,21 +13,21 @@ var content =
    name:       "Yaelin Hough",
    roles:      ["Lead Writer", "Researcher"],
    image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
-   statement:  'Growing up in Taiwan, I have always been happy and proud of my Taiwanese heritage. I am grateful for thie beautiful and unique culture, especially the yummy food like brown sugar bubble milk!<br>I wish everyone can respect and value foreing cultures as much as they value their own culture(s)!',
+   statement:  'I\'m proud of South Korea\'s ability to overcome its past hardships. Despite years under colonial rule and the subsequent destitution, South Korea has managed to cultivate a vibrant culture and a powerful democracy.<br>I hope that we are able to display the history and connections between all East and Southeast Asian cultures that exist beyond the common knowledge of the West.',
   },
   
   {
    type:       "Biography",
-   name:       "Janet Wu",
-   roles:      ["Executive Director", "Website Operator", "Writer", "Researcher"],
+   name:       "Amy Hwang",
+   roles:      ["Website Designer", "Writer", "Researcher"],
    image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
-   statement:  'Growing up in Taiwan, I have always been happy and proud of my Taiwanese heritage. I am grateful for thie beautiful and unique culture, especially the yummy food like brown sugar bubble milk!<br>I wish everyone can respect and value foreing cultures as much as they value their own culture(s)!',
+   statement:  '<b>Things I\'m proud of in my heritage:</b> Food, entertainment culture, and Korean culture of \'Jeong\'<br><b>What I want to convey through TFE:</b> To grow awareness of the current affairs in Asia and to be an advocate for my own country\'s culture(I wish to shwo that there are other appealing korean cultures other than the entertainment industry',
   },
   
   {
    type:       "Biography",
-   name:       "Janet Wu",
-   roles:      ["Executive Director", "Website Operator", "Writer", "Researcher"],
+   name:       "Kevin Yu",
+   roles:      ["Lead Web Designer", "Writer", "Researcher"],
    image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
    statement:  'Growing up in Taiwan, I have always been happy and proud of my Taiwanese heritage. I am grateful for thie beautiful and unique culture, especially the yummy food like brown sugar bubble milk!<br>I wish everyone can respect and value foreing cultures as much as they value their own culture(s)!',
   },
