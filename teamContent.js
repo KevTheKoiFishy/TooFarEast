8ff0ccf6d93494a955844e672407c6aa41ca1247
Glitch (glitch-hello-website) <none>
2023-01-18 22:20:46 +0000

    ☃️👯 Checkpoint ./teamContent.js:29312057/85

--- teamContent.js
+++ teamContent.js
@@ -10,8 +10,8 @@ var content =
   
   {
    type:       "Biography",
-   name:       "Janet Wu",
-   roles:      ["Executive Director", "Website Operator", "Writer", "Researcher"],
+   name:       "Yaelin Hough",
+   roles:      ["Lead Writer", "Researcher"],
    image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
    statement:  'Growing up in Taiwan, I have always been happy and proud of my Taiwanese heritage. I am grateful for thie beautiful and unique culture, especially the yummy food like brown sugar bubble milk!<br>I wish everyone can respect and value foreing cultures as much as they value their own culture(s)!',
   },
