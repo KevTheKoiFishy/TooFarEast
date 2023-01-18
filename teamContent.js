b95371af0b191b19d69a1dcbd0cbfd3177629735
Glitch (glitch-hello-website) <none>
2023-01-18 22:44:59 +0000

    🎪📰 Checkpoint ./teamContent.js:29312057/6688

--- teamContent.js
+++ teamContent.js
@@ -3,9 +3,9 @@ var content =
   {
    type:       "Biography",
    name:       "Janet Wu",
-   roles:      ["Executive Director", "Website Operator", "Writer", "Researcher"],
+   roles:      ["Executive Director", "Website Operator", "Social Media Manager", "Writer + Researcher"],
    image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
-   statement:  'Growing up in Taiwan, I have always been happy and proud of my Taiwanese heritage. I am grateful for thie beautiful and unique culture, especially the yummy food like brown sugar bubble milk!<br>I wish everyone can respect and value foreing cultures as much as they value their own culture(s)!',
+   statement:  '<b>Things I\'m proud of in my heritage:</b> Growing up in Taiwan, I have always been happy and proud of my Taiwanese heritage. I am grateful for thie beautiful and unique culture, especially the yummy food like brown sugar bubble milk!<br><b>What I want to convey through TFE:</b> I wish everyone can respect and value foreing cultures as much as they value their own culture(s)!',
   },
   
   {
@@ -13,7 +13,7 @@ var content =
    name:       "Yaelin Hough",
    roles:      ["Lead Writer", "Researcher"],
    image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
-   statement:  'I\'m proud of South Korea\'s ability to overcome its past hardships. Despite years under colonial rule and the subsequent destitution, South Korea has managed to cultivate a vibrant culture and a powerful democracy.<br>I hope that we are able to display the history and connections between all East and Southeast Asian cultures that exist beyond the common knowledge of the West.',
+   statement:  '<b>Things I\'m proud of in my heritage:</b> I\'m proud of South Korea\'s ability to overcome its past hardships. Despite years under colonial rule and the subsequent destitution, South Korea has managed to cultivate a vibrant culture and a powerful democracy.<br><b>What I want to convey through TFE:</b> I hope that we are able to display the history and connections between all East and Southeast Asian cultures that exist beyond the common knowledge of the West.',
   },
   
   {
@@ -29,7 +29,47 @@ var content =
    name:       "Kevin Yu",
    roles:      ["Lead Web Designer", "Writer", "Researcher"],
    image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
-   statement:  'Growing up in Taiwan, I have always been happy and proud of my Taiwanese heritage. I am grateful for thie beautiful and unique culture, especially the yummy food like brown sugar bubble milk!<br>I wish everyone can respect and value foreing cultures as much as they value their own culture(s)!',
+   statement:  '<b>Things I\'m proud of in my heritage:</b> Traditional Chinese stories about love, betrayal, and war.<br><b>What I want to convey through TFE:</b> China is much more than social credit memes or cheap goods. 1.4B people come with a plethora of microcosms, each with unique beliefs and problems. We need a more multifaceted view of the world\'s most populous country—not just world politics but life on the ground.',
+  },
+  
+  {
+   type:       "Biography",
+   name:       "Lily Wang",
+   roles:      ["Art and Design Director"],
+   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
+   statement:  '<b>Things I\'m proud of in my heritage:</b> Seeing people around the world appreciate Asian culture through food, media, music, landscape and beautiful traditions.<br><b>What I want to convey through TFE:</b> I want to show the public that countries are far more complex than most common impressions by providing through research.',
+  },
+  
+  {
+   type:       "Biography",
+   name:       "Oliver Chan",
+   roles:      ["Writer", "Researcher"],
+   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
+   statement:  '<b>Things I\'m proud of in my heritage:</b> Dim Sum, Mahjong, and Egg Waffle<br><b>What I want to convey through TFE:</b> I wish to share my expriences in Asian culture and the side of Asian culture that non-Asians may not have been exposed to.',
+  },
+  
+  {
+   type:       "Biography",
+   name:       "Jinnie Prapakamol",
+   roles:      ["Writer", "Researcher"],
+   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
+   statement:  '<b>Things I\'m proud of in my heritage:</b> Thai Food 🤤<br><b>What I want to convey through TFE:</b> Educating myself and other people about different cultures in the east asian community 🫡',
+  },
+  
+  {
+   type:       "Biography",
+   name:       "Leila Ng",
+   roles:      ["Social Media Assistant"],
+   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
+   statement:  '<b>Things I\'m proud of in my heritage:</b> My Fujianese heritage—family-centered, root centered.<br><b>What I want to convey through TFE:</b> The abundance of \'the Asian heritage.\' To make Asians of different communities and different upbringings feel heard.',
+  },
+  
+  {
+   type:       "Biography",
+   name:       "Nathan Leung",
+   roles:      ["Writer", "Researcher"],
+   image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/JaneyWoo",
+   statement:  '<b>Things I\'m proud of in my heritage:</b> Thai Food 🤤<br><b>What I want to convey through TFE:</b> Educating myself and other people about different cultures in the east asian community 🫡',
   },
   
 ];
