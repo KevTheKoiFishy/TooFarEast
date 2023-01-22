2ceb929f27c23876abcd0f84c492500230508f69
Glitch (glitch-hello-website) <none>
2023-01-22 05:27:12 +0000

    🐽💎 Checkpoint ./posts/0_SKYounger.html:29312057/48548 ./posts/2_ChinaZeroTolerance.html:29312057/5 ./posts/3_LetItRip.html:29312057/4 ./posts/4_LongFightToDemocracy.html:29312057/4 ./posts/1_180RohingyaDead.html:29312057/23572 ./postsContent.js:29312057/64

--- postsContent.js
+++ postsContent.js
@@ -7,7 +7,7 @@ var content =
    date:     "Jan 19 2023",
    image:    "https://www.marxist.com/images/stories/south_korea/2022_May/mourning_Image_May_18_Democratic_Uprising_Archive.jpg",
    para:     'Does everyone remember the Myanmar coup in 2021? The Myanmar coup d’état began on the morning of February 1st 2021. The National League for Democracy, the democratically elected leadership of the country, was deposed by the Tatmadaw, Myanmar’s military...<br>On the 16th of May 1961, Park Chung-hee and his allies took over the Korean government. Their dictatorship lasted twenty-seven years...',
-   buttons:  [["Read More", "/posts/4_LongFightToDemocracy.html"]],
+   buttons:  [["Read Full Article", "/posts/4_LongFightToDemocracy.html"]],
   },
   
   {
@@ -17,7 +17,7 @@ var content =
    date:     "Jan 11 2023",
    image:    "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iRR2kxsEz1X0/v0/1200x-1.jpg",
    para:     'January 8th marks the end of all COVID restrictions. The 387 passengers flying from Singapore and Toronto stepped onto the mainland without a PCR test, without a three week hotel quarantine.<br>But the sudden change in policy has left hospitals overwhelmed, medication stores emptied, and crematorium lines overflowing...',
-   buttons:  [["Read More", "/posts/3_LetItRip.html"]],
+   buttons:  [["Read Full Article", "/posts/3_LetItRip.html"]],
   },
   
   {
@@ -27,7 +27,7 @@ var content =
    date:     "Jan 3 2023",
    image:    "https://static.independent.co.uk/2022/10/19/11/2022-10-14T110118Z_1390418572_RC2Y0X9TSB0U_RTRMADP_3_CHINA-CONGRESS-PROTESTS-BEIJING-UGC.JPG",
    para:     'Written by an anonymous "Bridge Man," this October 13 poster encapsulates Chinese citizens\' growing dissent towards Xi and the Zero-COVID policy. While the western world has been accused of prioritizing economic profit over public health, China has done the opposite extreme...',
-   buttons:  [["Read More", "/posts/2_ChinaZeroTolerance.html"]],
+   buttons:  [["Read Full Article", "/posts/2_ChinaZeroTolerance.html"]],
   },
   
   {
@@ -37,7 +37,7 @@ var content =
    date:     "Dec 27 2022",
    image:    "https://www.amnesty.org/en/wp-content/uploads/2021/06/rohingya-refugees-1444x710.jpg",
    para:     'Earlier this week, a boat carrying over 100 Rohignya refugees disappeared at sea. The boat, which departed from Bangladesh, was most likely on its way to a Southeast Asian country. Although tragic...<br>The Rohingya are the people of a stateless, Indo-Aryan ethnic tribe who have historically resided in Myanmar. A Muslim tribe and an ethnic minority...',
-   buttons:  [["Read More", "/posts/1_180RohingyaDead.html"]],
+   buttons:  [["Read Post", "/posts/1_180RohingyaDead.html"]],
   },
   
   {
@@ -47,7 +47,7 @@ var content =
    date:     "Dec 20 2022",
    image:    "https://cdn.shopify.com/s/files/1/0185/3988/files/Screenshot_2020-12-03_at_10.21.08_1024x1024.png",
    para:     'On December 15th, 2022, South Korea passed a law that would adopt the international standard of age counting. South Korean used to have a traditional age system that when everyone is born at the age of 1 and will grow older every New Year. This previous age system has caused a huge amount of confusion...',
-   buttons:  [["Read More", "/posts/0_SKYounger.html"]],
+   buttons:  [["Read Post", "/posts/0_SKYounger.html"]],
   },
   
 ];
