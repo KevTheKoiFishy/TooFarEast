7141c6860cf8580552d6d8aa7ae9ca4225eb56fd
Glitch (glitch-hello-website) <none>
2023-01-31 23:11:17 +0000

    🌨🐂 Checkpoint ./indexSidebar.css:29312057/619 ./index.html:29312057/3397 ./indexSidebar.js:29312057/434

---
+++ indexSidebar.js
@@ -0,0 +1,18 @@
+<!-- 
+        China - Lily, Kevin
+        Mongolia - Yaelin 
+        Taiwan - Janet
+        South Korea - Amy, Yaelin
+        Japan - Lily, Nathan
+        Thailand - Jinnie
+        Philippines - Leila
+        Vietnam - Kevin
+        Indonesia - Kevin,Leila
+        Singapore - Yaelin, Nathan
+        Laos- Janet
+        Myanmar - Yaelin
+        Brunei-
+        Malaysia- Oliver, Nathan
+        Hong Kong - Nathan, Oliver
+        -->
+
