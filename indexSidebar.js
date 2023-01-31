afe7ee5144ccf738d5796f2313f02fe1096d59d2
Glitch (glitch-hello-website) <none>
2023-01-31 23:23:11 +0000

    😜🌯 Checkpoint ./indexSidebar.js:29312057/2004 ./.glitch-assets:29312057/3436

--- indexSidebar.js
+++ indexSidebar.js
@@ -1,18 +1,40 @@
-<!-- 
-        China - Lily, Kevin
-        Mongolia - Yaelin 
-        Taiwan - Janet
-        South Korea - Amy, Yaelin
-        Japan - Lily, Nathan
-        Thailand - Jinnie
-        Philippines - Leila
-        Vietnam - Kevin
-        Indonesia - Kevin,Leila
-        Singapore - Yaelin, Nathan
-        Laos- Janet
-        Myanmar - Yaelin
-        Brunei-
-        Malaysia- Oliver, Nathan
-        Hong Kong - Nathan, Oliver
+<!--
+        // China - Lily, Kevin
+        // Mongolia - Yaelin 
+        // Taiwan - Janet
+        // South Korea - Amy, Yaelin
+        // Japan - Lily, Nathan
+        // Thailand - Jinnie
+        // Philippines - Leila
+        // Vietnam - Kevin
+        // Indonesia - Kevin,Leila
+        // Singapore - Yaelin, Nathan
+        // Laos- Janet
+        // Myanmar - Yaelin
+        // Brunei-
+        // Malaysia- Oliver, Nathan
+        // Hong Kong - Nathan, Oliver
         -->
 
+var countries = ["China", "Mongolia", "Taiwan", "South Korea",
+                 "Japan", "Thailand", "Philippines", "Vietnam",
+                 "Indonesia", "Singapore", "Laos", "Myanmar",
+                 "Brunei", "Malaysia", "Hong Kong"
+                 ].sort();
+var subtopics = ["Food", "Music", "Dance", "Technology"];
+
+var sidebarDIV = document.getElementById("selectors");
+
+var HTMLToAdd = "";
+for (var c = 0; c < countries.length; c++)
+{
+  HTMLToAdd += '<div id="selector_'+countries[c]+'">';
+  HTMLToAdd += '<svg style="background: url(https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/'+countries[c]+');"></svg>';
+  HTMLToAdd += countries[c];
+  
+  for (var s = 0; s < subtopics.length; s++)
+    HTMLToAdd += '<div>' + subtopics[s] + '</div>';
+
+  HTMLToAdd += '</div>'
+}
+sidebarDIV.innerHTML += HTMLToAdd;
