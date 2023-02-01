be22427687d47f03076a39e6f0c4f1649670123d
Glitch (glitch-hello-website) <none>
2023-02-01 17:19:59 +0000

    🐮🐢 Checkpoint ./teamStyle.css:29312057/23 ./teamContent.js:29312057/121 ./team.html:29312057/118 ./posts/5_MoreThanTheMajority.html:29312057/65

--- teamContent.js
+++ teamContent.js
@@ -4,7 +4,7 @@ var content =
   {
    type:       "Biography",
    name:       "Janet Wu ",
-   roles:      ["Executive Director", "Website Operator", "Social Media Manager", "Writer + Researcher"],
+   roles:      ["Executive Director", "Social Media Manager", "Website Operator", "Writer + Researcher"],
    image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/Janet",
    statement:  '<b>Things I\'m proud of in my heritage:</b> Growing up in Taiwan, I have always been happy and proud of my Taiwanese heritage. I am grateful for thie beautiful and unique culture, especially the yummy food like brown sugar bubble milk!<br><b>What I want to convey through TFE:</b> I wish everyone can respect and value foreing cultures as much as they value their own culture(s)!',
   },
@@ -28,7 +28,7 @@ var content =
   {
    type:       "Biography",
    name:       "Kevin Yu 于笑涵",
-   roles:      ["Lead Web Designer", "Writer", "Researcher"],
+   roles:      ["Lead Web Designer", "Writer + Researcher", "Chief Editor"],
    image:      "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/Kevin",
    statement:  '<b>Things I\'m proud of in my heritage:</b> Traditional Chinese stories about love, betrayal, and war.<br><b>What I want to convey through TFE:</b> China is much more than social credit memes or cheap goods. 1.4B people come with a plethora of microcosms, each with unique beliefs and problems. We need a more multifaceted view of the world\'s most populous country—not just world politics but life on the ground.',
   },
