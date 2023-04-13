dbab216281d8e8462480d24ee953e17316e2d482
Glitch (glitch-hello-website) <none>
2023-04-13 05:00:47 +0000

    🏮👞 Checkpoint ./index.html:29312057/1484 ./htmlblocks/header.js:29312057/2328 ./htmlblocks/footer.js:29312057/844

---
+++ htmlblocks/footer.js
@@ -0,0 +1,18 @@
+document.body.innerHTML += 
+`
+<div class="header">
+  <a class="active" href="/">              Welcome       </a>
+  <a class="      " href="/posts.html">    Posts         </a>
+  <a class="      " href="/team.html">     Meet the Team </a>
+  <a class="      " href="/contact.html">  Contact Us    </a>
+</div>
+<marquee id="LatestNews" scrollmount="2"></marquee>
+
+<div id="sidebar">
+  <div id="sunset"></div>
+  <div id="logo"><div id="logoIMG"></div></div>
+  <div id="selectors">
+  <div style="transform: translateX(-20px); width: 300px">🎓Learn About Asia!</div>
+  </div>
+</div>
+`
