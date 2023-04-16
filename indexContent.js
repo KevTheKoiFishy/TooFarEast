5b9d345575e925e6cb49322c675b7e93d5534af6
Glitch (glitch-hello-website) <none>
2023-04-16 23:19:04 +0000

    🏗🎂 Checkpoint ./indexContent.js:29312057/4 ./fundraise.html:29312057/18

--- indexContent.js
+++ indexContent.js
@@ -13,6 +13,6 @@ var content =
    title:   "What We Do",
    image:   "https://cdn.glitch.global/2f875abb-5388-450d-8613-bd657c26368b/990193a214ac8d1c01cd287f843258e6.jpg",
    para:    'As a multicultural organization, Too Far East hopes to increase communication using our informative blog posts and passionate opinions. All Asian countries have voices that need to be heard. Using in-depth research and personal experiences as both native Asians and Asian Americans, we hope to provide the knowledge and resources to form a more multifaceted worldview.',
-   buttons: [["Extra Extra!", "posts.html"], ["These Orgs are Looking for Help", "fundraising.html"]],
+   buttons: [["Extra Extra!", "posts.html"], ["These Orgs are Looking for Help", "fundraise.html"]],
   },
 ];
