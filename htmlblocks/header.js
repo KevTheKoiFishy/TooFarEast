9c6d5492733099ba35eb8f8950cb662881777967
Glitch (glitch-hello-website) <none>
2023-04-18 13:36:03 +0000

    🍦🎏 Checkpoint ./posts/index.html:29312057/29 ./fundraise/index.html:29312057/120 ./regions.html:29312057/33 ./team.html:29312057/35 ./htmlblocks/header.js:29312057/944 ./indexContent.js:29312057/10

--- htmlblocks/header.js
+++ htmlblocks/header.js
@@ -11,11 +11,12 @@ window.addEventListener("resize", zoomBody);
 document.body.innerHTML +=
 `
 <div class="header">
-  <a class="` + (window.location.pathname.replaceAll(".html", "").split("/")[1] == ""        ? "active" : "inactive") + `" href="/">             Welcome       </a>
-  <a class="` + (window.location.pathname.replaceAll(".html", "").split("/")[1] == "posts"   ? "active" : "inactive") + `" href="/posts.html">   Posts         </a>
-  <a class="` + (window.location.pathname.replaceAll(".html", "").split("/")[1] == "team"    ? "active" : "inactive") + `" href="/team.html">    Meet the Team </a>
-  <a class="` + (window.location.pathname.replaceAll(".html", "").split("/")[1] == "regions" ? "active" : "inactive") + `" href="/regions.html"> Meet Asia     </a>
-  <a class="` + (window.location.pathname.replaceAll(".html", "").split("/")[1] == "contact" ? "active" : "inactive") + `" href="/contact.html"> Contact Us    </a>
+  <a class="` + (window.location.pathname.replaceAll(".html", "").split("/")[1] == ""           ? "active" : "inactive") + `" href="/">          Welcome       </a>
+  <a class="` + (window.location.pathname.replaceAll(".html", "").split("/")[1] == "posts"      ? "active" : "inactive") + `" href="/posts">     Posts         </a>
+  <a class="` + (window.location.pathname.replaceAll(".html", "").split("/")[1] == "regions"    ? "active" : "inactive") + `" href="/regions">   Meet Asia     </a>
+  <a class="` + (window.location.pathname.replaceAll(".html", "").split("/")[1] == "team"       ? "active" : "inactive") + `" href="/team">      Meet the Team </a>
+  <a class="` + (window.location.pathname.replaceAll(".html", "").split("/")[1] == "contact"    ? "active" : "inactive") + `" href="/contact">   Contact Us    </a>
+  <a class="` + (window.location.pathname.replaceAll(".html", "").split("/")[1] == "fundraise"  ? "active" : "inactive") + `" href="/fundraise"> Support       </a>
 </div>
 <marquee id="LatestNews" scrollmount="2"></marquee>
 
