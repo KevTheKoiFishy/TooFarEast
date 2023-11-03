5d4a65a4ff96f4f4d32e2922b3128a3d510bd202
KevTheKoiFishy <ky5429560942@gmail.com>
2023-11-03 16:30:08 -0400

    ​

--- hidden/birthday_RosPsBUFaE/birthdayContent.js
+++ hidden/birthday_RosPsBUFaE/birthdayContent.js
@@ -9,4 +9,4 @@ var content =
    para:    "Janet,<br>&emsp;For someone so playfully chaotic, you've been such a stable source of fun and support for me. I'm really happy that out of all the parallel universes, I ended up in one where we went to the same middle-of-nowhere school for two years.<br>&emsp;My last year at Kent would have been SOOOO boring without Too Far East and Innovation Club and Dance. Every time I see kpop dance groups on campus my mind goes to you and Amy. Legit the most Korean people I know (not yaelin). I miss you lotsss <3 Noone at my new school comes close to your spirit. <br>&emsp;Also, damn girl, you're so olddddddd. I am giving you the asian style Big Bow and saying 身体健康、万事如意、and I hope your life is as long as all the ramen you've eaten strung end-to-end. Now where's my red envelope O-o.<br>&emsp;Sending you birthday wishes, and I'd really love to be there today TT.<br><br>See you in a few months,<br>Koi Fishy",
    buttons: [],
   },
-];
+]; 
